db490af7e29ecec5eb1006144f4e6bffbbe5b75f
kwokcheong <wongkwokcheong@hotmail.com>
2020-07-16 16:38:57 +0800

    Added validations to rails and introduced user model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -19,9 +19,11 @@ class ProductsController < ApplicationController
         @product = Product.new(product_params)
         if @product.save
             # redirect_to @product
+            flash.now[:notice] = "Product has been saved"
             redirect_to root_path
         else 
-            render 'new'
+            flash.now[:alert] = "Product has not been saved"
+            render :new
         end
     end
 
@@ -31,8 +33,13 @@ class ProductsController < ApplicationController
 
     def update
         # @product = Product.find(params[:id])
-        @product.update(product_params)
-        redirect_to root_path
+        if @product.update(product_params)
+            flash.now[:notice] = "Product has been updated"
+            redirect_to root_path
+        else 
+            flash.now[:alert] = "Product has not been updated"
+            render :edit
+        end
     end
 
     def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
-    validates :name, present: true
+
+    validates :name, presence: true, allow_nil: false
+    validates :description, presence: true, length: {minimum: 5}, allow_nil: false
+    validates :price, numericality: {
+        greater_than_or_equal_to: 0.0
+    }
 end
+
